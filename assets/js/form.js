bdb42d3a5f8342e8e4be78c977953a624ae6e955
Alejandro Martinez <alemar@Alejandros-MacBook-Pro.local>
2024-04-24 21:29:06 -0700

    Finished adding dark mode styles to form.css and completed code for dark mode in form.js.

--- assets/js/form.js
+++ assets/js/form.js
@@ -12,3 +12,25 @@ const submitFormInput = function() {
 }
 
 formBtn.addEventListener( "click", submitFormInput );
+
+darkModeToggler.addEventListener( "click", function( event ) {
+  if( isDarkMode ) {
+    modeWrapper.classList.remove( "dm-mode-wrapper" );
+    sunIcon.classList.remove( "hide" );
+    moonIcon.classList.add( "hide" );
+    document.body.classList.remove( "dm-body" );
+    darkModeToggler.classList.remove( "dm-dark-mode-toggler" );
+    landingIntroArea.classList.remove( "dm-landing-intro-area" );
+    formBtn.classList.remove( "dm-form-btn" );
+    isDarkMode = false;
+  } else {
+    modeWrapper.classList.add( "dm-mode-wrapper" );
+    sunIcon.classList.add( "hide" );
+    moonIcon.classList.remove( "hide" );
+    document.body.classList.add( "dm-body" );
+    darkModeToggler.classList.add( "dm-dark-mode-toggler" );
+    landingIntroArea.classList.add( "dm-landing-intro-area" );
+    formBtn.classList.add( "dm-form-btn" );
+    isDarkMode = true;
+  }
+} );
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,16 @@
 
+const darkModeToggler = document.querySelector( ".dark-mode-toggler" );
+const modeWrapper = document.querySelector( ".mode-wrapper" );
+const sunIcon = document.querySelector( ".sun-icon" );
+const moonIcon = document.querySelector( ".moon-icon" );
+const landingIntroArea = document.querySelector( ".landing-intro-area" );
 const usernameInput = document.querySelector( "#username" );
 const titleInput = document.querySelector( "#title" );
 const contentInput = document.querySelector( "#content" );
 const formBtn = document.querySelector( ".form-btn" );
 const postsContainer = document.querySelector( ".posts-container" );
 const postsArray = [];
+let isDarkMode = false;
 
 if( localStorage.getItem( "posts" ) ) {
   const dataArray = JSON.parse( localStorage.getItem( "posts" ) );
